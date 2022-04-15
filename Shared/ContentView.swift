030a064454e4b10ab9fedb44e0d066f227e428d8
Katelyn Lydeen <klydeen@hawk.iit.edu>
2022-04-15 17:20:40 -0500

    Initial Commit

---
+++ Shared/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Shared
+//
+//  Created by Katelyn Lydeen on 4/15/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Shared/Wang_Landau_SamplingApp.swift
@@ -0,0 +1,17 @@
+//
+//  Wang_Landau_SamplingApp.swift
+//  Shared
+//
+//  Created by Katelyn Lydeen on 4/15/22.
+//
+
+import SwiftUI
+
+@main
+struct Wang_Landau_SamplingApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
